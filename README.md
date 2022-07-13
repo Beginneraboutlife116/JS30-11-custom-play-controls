ab5a451a1e456d19fa1efa4a67e3bdbcaee60676
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-07-13 18:42:40 +0800

    increase basic play function and style

---
+++ README.md
@@ -0,0 +1,8 @@
+# TODO
+- [ ] Initial style
+- [ ] Play/pause
+- [ ] Mute
+- [ ] Volume control by slider
+- [ ] Progress bar
+- [ ] Skip ahead without button, try to mimic youtube
+- [ ] Go fullScreen
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(".player")
 const video = document.querySelector(".player__video")
+const poster = document.querySelector(".player__poster")
 const controls = document.querySelector(".player__controls")
 const progress = document.querySelector(".progress")
 const progressBar = document.querySelector(".progress__bar")
@@ -10,3 +11,29 @@ const settingBtn = document.querySelector(".setting")
 const dialog = document.querySelector(".dialog")
 const playbackRateSlider = document.querySelector("input[name='playbackRate']")
 const fullscreen = document.querySelector(".fullscreen")
+
+//* Functions
+function playOrPauseVideo() {
+  poster.dataset.state = "hidden"
+  const currentState = video.paused || video.ended ? "play" : "pause"
+  video[currentState]()
+  changeButtonState("playOrPause")
+}
+
+function changeButtonState(type) {
+  if (type === "playOrPause") {
+    playOrPauseBtn.dataset.state =
+      video.paused || video.ended ? "play" : "pause"
+  }
+}
+
+//* Program
+const supportsVideo = !!document.createElement("video").canPlayType
+
+if (supportsVideo) {
+  video.controls = false
+  controls.dataset.state = "visible"
+}
+
+video.addEventListener("click", playOrPauseVideo)
+playOrPauseBtn.addEventListener("click", playOrPauseVideo)
