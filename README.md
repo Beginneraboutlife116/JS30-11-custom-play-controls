d9853814bf7ee74417a9cdcecdc77c4fd70d7211
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-07-14 12:02:18 +0800

    finish volume controls

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 - [x] Initial style
 - [x] Play/pause
-- [ ] Mute
-- [ ] Volume control by slider
+- [x] Mute
+- [x] Volume control by slider
+- [ ] Playback rate control by slider
 - [ ] Progress bar
 - [ ] Skip ahead without button, try to mimic youtube
 - [ ] Go fullScreen
--- main.js
+++ main.js
@@ -6,10 +6,8 @@ const progress = document.querySelector(".progress")
 const progressBar = document.querySelector(".progress__bar")
 const playOrPauseBtn = document.querySelector(".play-pause")
 const volumeBtn = document.querySelector(".volume")
-const volumeSlider = document.querySelector("input[id='volume']")
 const settingBtn = document.querySelector(".setting")
 const dialog = document.querySelector(".dialog")
-const playbackRateSlider = document.querySelector("input[name='playbackRate']")
 const fullscreen = document.querySelector(".fullscreen")
 
 //* Functions
@@ -25,14 +23,64 @@ function changeButtonState(type) {
     playOrPauseBtn.dataset.state =
       video.paused || video.ended ? "play" : "pause"
   }
-  if (type === 'volume') {
-    if (video.muted || video.volume === 0) {
-      volumeBtn.dataset.state = 'mute'
-    } else if (video.volume <= 0.5) {
-      volumeBtn.dataset.state = 'medium'
+  if (type === "volume") {
+    if (video.muted) {
+      volumeBtn.dataset.state = "mute"
+    } else if (video.volume === 0) {
+      volumeBtn.dataset.state = "mute"
     } else {
-      volumeBtn.dataset.state = 'large'
+      volumeBtn.dataset.state = "sound"
     }
+  }
+}
+
+//* Class
+class Slider {
+  #mouseDown = false
+  constructor({ htmlElement, mediaAPI }) {
+    this.htmlElement = htmlElement
+    this.mediaAPI = mediaAPI
+    this.step = Number.parseFloat(htmlElement.step)
+    this.min = Number.parseFloat(htmlElement.min)
+    this.max = Number.parseFloat(htmlElement.max)
+    this.value = Number.parseFloat(htmlElement.value)
+  }
+
+  mouseEvent() {
+    this.htmlElement.addEventListener("mousedown", () => {
+      this.#mouseDown = true
+    })
+    this.htmlElement.addEventListener("mousemove", (event) => {
+      if (!this.#mouseDown) return
+      const value = Number.parseFloat(event.target.value)
+      video[this.mediaAPI] = value.toString()
+    })
+    this.htmlElement.addEventListener("mouseup", (event) => {
+      const value = Number.parseFloat(event.target.value)
+      video[this.mediaAPI] = value.toString()
+      this.#mouseDown = false
+    })
+  }
+
+  changeEvent() {
+    this.htmlElement.addEventListener("change", (event) => {
+      const value = Number.parseFloat(event.target.value)
+      video[this.mediaAPI] = value.toString()
+    })
+  }
+
+  wheelEvent() {
+    this.htmlElement.addEventListener("wheel", (e) => {
+      if (e.deltaY > 0) {
+        this.value += this.step
+        this.value = this.value >= this.max ? this.max : this.value
+      } else if (e.deltaY < 0) {
+        this.value -= this.step
+        this.value = this.value <= this.min ? this.min : this.value
+      }
+      this.htmlElement.value = this.value.toString()
+      video[this.mediaAPI] = this.value.toString()
+    })
   }
 }
 
@@ -46,7 +94,24 @@ if (supportsVideo) {
 
 video.addEventListener("click", playOrPauseVideo)
 playOrPauseBtn.addEventListener("click", playOrPauseVideo)
-volumeBtn.addEventListener('click', () => {
+volumeBtn.addEventListener("click", () => {
   video.muted = !video.muted
-  changeButtonState('volume')
+  changeButtonState("volume")
+})
+
+const volumeSlider = new Slider({
+  htmlElement: document.querySelector("input[id='volume']"),
+  mediaAPI: "volume"
+})
+const playbackRateSlider = new Slider({
+  htmlElement: document.querySelector("input[name='playbackRate']"),
+  mediaAPI: "playbackRate"
+})
+
+volumeSlider.mouseEvent()
+volumeSlider.changeEvent()
+volumeSlider.wheelEvent()
+
+video.addEventListener("volumechange", () => {
+  changeButtonState("volume")
 })
